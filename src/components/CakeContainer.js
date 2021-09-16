afdfa25516cd865e9f15d3db2ffc83a793660ded
Dell <nayanakanchana9@gmail.com>
2021-09-16 17:04:27 +0530

    redux app

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {connect} from 'react-redux'
+import {buyCake} from '../redux'
+
+function CakeContainer(props) {
+  return (
+    <div>
+      <h2>Cakes - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy</button>
+    </div>
+  );
+}
+
+const mapStateToProp = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProp, mapDispatchtoProps) (CakeContainer);
